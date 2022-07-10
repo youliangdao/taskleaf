9320d5efd933a86d0e742a57c145920558d1c723
youliangdao <youliangdaojing5@gmail.com>
2022-07-10 11:45:52 +0900

    データ制限

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_09_021703) do
+ActiveRecord::Schema.define(version: 2022_07_10_022913) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
