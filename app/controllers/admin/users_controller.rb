f88a303c8b94205cb415b49940c2e61e61c2189b
youliangdao <youliangdaojing5@gmail.com>
2022-07-11 20:57:49 +0900

    ユーザー管理機能の追加

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,33 @@
+class Admin::UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users =User.all
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+end
